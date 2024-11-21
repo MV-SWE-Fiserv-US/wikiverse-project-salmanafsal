af6ad92ff6343228b9721b8398f04c147d5a1a8a
salman.afzal <salman.afzal@clover.com>
2024-11-21 10:49:29 -0500

    singlePageView

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -6,15 +6,32 @@ import apiURL from '../api'
 
 export const App = () => {
   const [pages, setPages] = useState([])
-
+  const [selectedArticle, setSelectedArticle]=useState(null)
   async function fetchPages () {
     try {
       const response = await fetch(`${apiURL}/wiki`)
+      
       const pagesData = await response.json()
+    
       setPages(pagesData)
     } catch (err) {
       console.log('Oh no an error! ', err)
     }
+  }
+
+  async function fetchArticle(slug) {
+    try {
+      const response = await fetch(`http://localhost:3000/api/wiki/${slug}`);
+      const articleData = await response.json();
+      console.log(`${slug}`)
+      setSelectedArticle(articleData);
+    } catch (err) {
+      console.log('Error fetching article:', err);
+    }
+  }
+
+  function handleBackToList() {
+    setSelectedArticle(null);
   }
 
   useEffect(() => {
@@ -25,7 +42,20 @@ export const App = () => {
 		<main>
       <h1>WikiVerse</h1>
 			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+			{selectedArticle ? (
+      <div>
+        <h3>{selectedArticle.title}</h3>
+       
+        <p><strong>Author:</strong> {selectedArticle.author.name}</p>
+        <p><strong>Content:</strong> {selectedArticle.content}</p>
+        <p><strong>Tags:</strong> {selectedArticle.tags.map(tag => tag.name).join(', ')}</p>
+        <p><strong>Date:</strong> {new Date(selectedArticle.createdAt).toLocaleDateString()}</p>
+        <button onClick={handleBackToList}>Back to Wiki List</button>
+      </div>
+    ) : (
+      
+      <PagesList pages={pages} onArticleClick={fetchArticle} />
+    )}
 		</main>
   )
 }
--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Page } from './Page'
 
-export const PagesList = ({ pages }) => {
+export const PagesList = ({ pages,onArticleClick }) => {
   return <>
-		{
-			pages.map((page, idx) => {
-				return <Page page={page} key={idx} />
-			})
-		}
+		{pages.map((page,index) => (
+        <li key={index} onClick={() => onArticleClick(page.slug)}>
+          <h3>{page.title}</h3>
+		  <p>{page.slug}</p>
+        </li>
+      ))}
 	</>
 }
